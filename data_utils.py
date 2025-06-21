2307ce33d41ea204c490df31ee7f24611d348818
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-06-21 10:29:12 +0900

    normalization 되돌림

--- data_utils.py
+++ data_utils.py
@@ -40,8 +40,7 @@ class TextMelLoader(torch.utils.data.Dataset):
             if sampling_rate != self.stft.sampling_rate:
                 raise ValueError("{} SR doesn't match target {} SR".format(
                     sampling_rate, self.stft.sampling_rate))
-            #audio_norm = audio / self.max_wav_value
-            audio_norm = audio ###
+            audio_norm = audio / self.max_wav_value
             audio_norm = audio_norm.unsqueeze(0)
             audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
             melspec = self.stft.mel_spectrogram(audio_norm)
