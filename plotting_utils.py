92388362af61bfab3818e7dc6d4d145b49e9ef57
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-06-21 10:43:53 +0900

    tostring_rgb -> tostring_argb

--- plotting_utils.py
+++ plotting_utils.py
@@ -7,7 +7,7 @@ import numpy as np
 def save_figure_to_numpy(fig):
     # save it to a numpy array.
     fig.canvas.draw()
-    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = np.frombuffer(fig.canvas.tostring_argb(), dtype=np.uint8)
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     return data
 
