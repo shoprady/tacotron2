b3b6c6912af731180132b6503bee876915d5b14a
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-06-21 13:02:55 +0900

    remove debug message

--- data_utils.py
+++ data_utils.py
@@ -37,8 +37,11 @@ class TextMelLoader(torch.utils.data.Dataset):
     def get_mel(self, filename):
         if not self.load_mel_from_disk:
             audio, sampling_rate = load_wav_to_torch(filename)
+            if audio.ndim == 2:
+                # Stereo audio detected -> mono
+                audio = torch.mean(audio, axis=1) # (T, 2) -> (T,)
             if sampling_rate != self.stft.sampling_rate:
-                raise ValueError("{} {} SR doesn't match target {} SR".format(
+                raise ValueError("{} SR doesn't match target {} SR".format(
                     sampling_rate, self.stft.sampling_rate))
             audio_norm = audio / self.max_wav_value
             audio_norm = audio_norm.unsqueeze(0)
